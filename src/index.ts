e6c3232b87da03c65cf92b8004e37f0f5979fb10
d1r0l <xd1r0lx@gmail.com>
2023-10-14 17:16:38 +0400

    getting cookies completed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import bot from './bot';
+import getIgCookies from './utils/getIgCookies';
+
+getIgCookies().catch(console.error);
 
 bot().catch(console.error);
--- src/utils/getIgCookies.ts
+++ src/utils/getIgCookies.ts
@@ -1,4 +1,9 @@
+import dotenv from 'dotenv';
 import axios from 'axios';
+import _ from 'lodash';
+import sodium from 'libsodium-wrappers';
+import crypto from 'node:crypto';
+dotenv.config();
 
 const baseurl = 'https://www.instagram.com/api/v1';
 const userAgent =
@@ -7,15 +12,7 @@ const userAgent =
   'Mobile/15E148 Instagram 142.0.0.22.109 ' +
   '(iPhone12,5; iOS 14_1; en_US; en-US; scale=3.00; 1242x2688; 214888322) NW/1';
 
-const isString = (value: unknown): value is string => {
-  return typeof value === 'string';
-};
-
-const isObject = (value: unknown): value is object => {
-  return typeof value === 'object' && value !== null;
-};
-
-export const getPublicKeyData = async (): Promise<
+const getPublicKeysData = async (): Promise<
   | {
       csrfToken: string;
       publicKeyId: string;
@@ -35,19 +32,19 @@ export const getPublicKeyData = async (): Promise<
 
   if (
     res.status === 200 &&
-    isObject(res.data) &&
+    _.isObject(res.data) &&
     'config' in res.data &&
-    isObject(res.data.config) &&
+    _.isObject(res.data.config) &&
     'csrf_token' in res.data.config &&
-    isString(res.data.config.csrf_token) &&
+    _.isString(res.data.config.csrf_token) &&
     'encryption' in res.data &&
-    isObject(res.data.encryption) &&
+    _.isObject(res.data.encryption) &&
     'key_id' in res.data.encryption &&
-    isString(res.data.encryption.key_id) &&
+    _.isString(res.data.encryption.key_id) &&
     'public_key' in res.data.encryption &&
-    isString(res.data.encryption.public_key) &&
+    _.isString(res.data.encryption.public_key) &&
     'version' in res.data.encryption &&
-    isString(res.data.encryption.version)
+    _.isString(res.data.encryption.version)
   ) {
     const csrfToken = res.data.config.csrf_token;
     const publicKeyId = res.data.encryption.key_id;
@@ -63,3 +60,95 @@ export const getPublicKeyData = async (): Promise<
     return undefined;
   }
 };
+
+const encryptPassword = async (
+  pKeyId: string,
+  pKey: string,
+  pKeyVersion: string,
+  pass: string,
+): Promise<string> => {
+  const time = Math.floor(Date.now() / 1000).toString();
+
+  const key = await crypto.subtle.generateKey(
+    {
+      name: 'AES-GCM',
+      length: 256,
+    },
+    true,
+    ['encrypt', 'decrypt'],
+  );
+  const sealedKey = sodium.crypto_box_seal(
+    Buffer.from(await crypto.subtle.exportKey('raw', key)),
+    Buffer.from(pKey, 'hex'),
+  );
+
+  const sizeBuffer = Buffer.alloc(2, 0);
+  sizeBuffer.writeInt16LE(sealedKey.byteLength, 0);
+
+  const encPass = await crypto.subtle.encrypt(
+    {
+      name: 'AES-GCM',
+      iv: Buffer.alloc(12, 0),
+      additionalData: Buffer.from(time, 'utf8'),
+    },
+    key,
+    Buffer.from(pass, 'utf8'),
+  );
+
+  const payload = Buffer.concat([
+    Buffer.alloc(1, 1),
+    Buffer.alloc(1, pKeyId),
+    Buffer.from(sizeBuffer),
+    Buffer.from(sealedKey),
+    Buffer.from(encPass, Buffer.from(encPass).length - 16, 16),
+    Buffer.from(encPass, 0, Buffer.from(encPass).length - 16),
+  ]).toString('base64');
+
+  const result = ['#PWD_INSTAGRAM_BROWSER', pKeyVersion, time, payload].join(
+    ':',
+  );
+  return result;
+};
+
+const getIgCookies = async () => {
+  if (!process.env.IG_USERNAME || !process.env.IG_PASSWORD)
+    throw new Error('No Instagram username or password provided.');
+
+  const url = `${baseurl}/web/accounts/login/ajax/`;
+  const keys = await getPublicKeysData();
+  if (!keys) throw new Error('Cannot get public keys data.');
+
+  const enc_password = await encryptPassword(
+    keys.publicKeyId,
+    keys.publicKey,
+    keys.publicKeyVersion,
+    process.env.IG_PASSWORD,
+  );
+
+  const req = {
+    url: url,
+    method: 'POST',
+    headers: {
+      'User-Agent': userAgent,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Cookie: `csrftoken=${keys.csrfToken}`,
+      'X-Csrftoken': keys.csrfToken,
+    },
+    data: {
+      enc_password: enc_password,
+      optIntoOneTap: false,
+      queryParams: '{}',
+      trustedDeviceRecords: '{}',
+      username: process.env.IG_USERNAME,
+    },
+    validateStatus: () => true,
+  };
+
+  const res = await axios(req);
+  if (res.status !== 200) throw new Error('Cannot get Instagram cookies.');
+  if (!res.headers['set-cookie']) throw new Error('Cookies not found.');
+  const cookies = res.headers['set-cookie'];
+  console.log(cookies);
+};
+
+export default getIgCookies;
