ee4e2ec4766ee9de8d4507017544e6aaf6ccdd6a
d1r0l <xd1r0lx@gmail.com>
2024-01-11 12:39:25 +0200

    loadCorsMediaRouter refactor

--- server/src/routes/loadCorsMediaRouter.ts
+++ server/src/routes/loadCorsMediaRouter.ts
@@ -1,25 +1,37 @@
 import express from 'express';
 import _ from 'lodash';
 import axios from 'axios';
+
 import regex from '../utils/regex';
 
 const loadCorsMediaRouter = express.Router();
 
 loadCorsMediaRouter.get('/', (async (req, res) => {
-  if ('query' in req && _.isObject(req.query)) {
-    const query = req.query;
-    if ('url' in query && _.isString(query.url)) {
-      const url = Buffer.from(query.url, 'base64').toString('ascii');
-      if (regex.igDbLink.test(url)) {
-        const igRes = await axios({
-          url: url,
-          method: 'GET',
-          responseType: 'arraybuffer',
-        });
-        res.status(igRes.status).header(igRes.headers).send(igRes.data);
-      } else res.status(400).send({ error: 'Bad url' });
-    } else res.status(400).send({ error: 'No url provided' });
-  } else res.status(400).send({ error: 'No query provided' });
+  if (!('query' in req) || !_.isObject(req.query)) {
+    res.status(400).send({ error: 'No query provided' });
+    return;
+  }
+
+  const query = req.query;
+
+  if (!('url' in query) || !_.isString(query.url)) {
+    res.status(400).send({ error: 'No url provided' });
+    return;
+  }
+
+  const url = Buffer.from(query.url, 'base64').toString('ascii');
+
+  if (!regex.igDbLink.test(url)) {
+    res.status(400).send({ error: 'Bad url' });
+    return;
+  }
+
+  const igRes = await axios({
+    url: url,
+    method: 'GET',
+    responseType: 'arraybuffer',
+  });
+  res.status(igRes.status).header(igRes.headers).send(igRes.data);
 }) as express.RequestHandler);
 
 export default loadCorsMediaRouter;
