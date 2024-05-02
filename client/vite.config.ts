b16957a8b236fe6e8473950fdd5ea2c639018343
d1r0l <xd1r0lx@gmail.com>
2024-05-02 16:25:54 +0300

    feat: :sparkles: added PWA config and features

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://getinstamedia.d1r0l.xyz',
+  base: 'https://getinstamedia.d1r0l.xyz',
   build: {
     outDir: 'build'
   },
