7b0dea9d195894019cb1fa8bc57dd853c590e0ab
d1r0l <xd1r0lx@gmail.com>
2024-05-02 17:08:56 +0300

    build: :package: added pwa-vite plugin for seamless testing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   base: 'https://getinstamedia.d1r0l.xyz',
   build: {
     outDir: 'build'
