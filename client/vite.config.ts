858cb10f4ddc7312404e8d294f6b819df27b7c2b
d1r0l <xd1r0lx@gmail.com>
2024-05-08 18:20:57 +0300

    build: :hammer: dev server https setup
    added https client and server setup for seamless testing and improved logging

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import mkcert from 'vite-plugin-mkcert'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    mkcert({ savePath: '../certs' }),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   base: 'https://getinstamedia.d1r0l.xyz',
   build: {
     outDir: 'build'
   },
   server: {
-    port: 3001
+    port: 3001,
+    https: true,
+    proxy: {
+      '/api': {
+        target: 'https://192.168.1.5:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
