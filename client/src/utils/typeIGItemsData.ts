7b92c3f070e37f97e9e8b4e9f2f13c98174854f2
d1r0l <xd1r0lx@gmail.com>
2024-05-17 18:17:38 +0300

    fix: :bug: fixed merging error

--- client/src/utils/typeIGItemsData.ts
+++ client/src/utils/typeIGItemsData.ts
@@ -2,11 +2,7 @@ import { IGItemData, Media } from '../types'
 import { baseUrl } from './config'
 import axios from 'axios'
 import extToMime from './extToMime'
-import { baseUrl } from './config'
-import axios from 'axios'
-import extToMime from './extToMime'
 
-const typeIGItemsData = async (
 const typeIGItemsData = async (
   itemsData: { name: string; medias: string[] }[]
 ): Promise<IGItemData[]> => {
@@ -23,31 +19,12 @@ const typeIGItemsData = async (
     }
   }
 
-): Promise<IGItemData[]> => {
-  const fetchBlobUrl = async (url: string) => {
-    try {
-      const res = await axios.get(`${baseUrl}/api/proxy`, {
-        responseType: 'blob',
-        params: { url: btoa(url) }
-      })
-      return URL.createObjectURL(res.data)
-    } catch (error) {
-      console.error(error)
-      return ''
-    }
-  }
-
   const typedIGItemsData: IGItemData[] = []
-  for await (const item of itemsData) {
   for await (const item of itemsData) {
     const newItemData: IGItemData = {
       name: item.name,
-      name: item.name,
       medias: []
     }
-    for await (const media of item.medias) {
-      const extMatch = media.match(/\.([^/\s]*)\?/i)
-      const extention = extMatch ? extMatch[1].toLowerCase() : ''
     for await (const media of item.medias) {
       const extMatch = media.match(/\.([^/\s]*)\?/i)
       const extention = extMatch ? extMatch[1].toLowerCase() : ''
