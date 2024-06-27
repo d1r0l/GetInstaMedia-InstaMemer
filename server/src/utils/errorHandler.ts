8d8fb6992fc6b5516cc29a4c84c1fbb1aca034b5
d1r0l <xd1r0lx@gmail.com>
2024-06-27 15:46:03 +0300

    feat: :goal_net: updated errorHandler

--- server/src/utils/errorHandler.ts
+++ server/src/utils/errorHandler.ts
@@ -1,16 +1,27 @@
 import { nanoid } from 'nanoid';
 import { envMode } from '../utils/config';
+import fs from 'fs';
 
 const errorHandler = (error: unknown) => {
   if (envMode === 'production') {
-    if (error instanceof Error) console.error('Error: ' + error.name);
+    if (error instanceof Error)
+      console.error(`Error ${error.name}: ${error.message}`);
     else console.log('Something went wrong.');
   } else {
     if (error instanceof Error) {
       const errorId = nanoid();
-      console.error(`<========== Error ${errorId} start ==========>`);
-      console.error(error);
-      console.error(`<=========== Error ${errorId} end ===========>`);
+
+      console.error(`Error ${errorId}`);
+
+      const filename = `${error.name}_${errorId}.txt`;
+
+      let parsedError = `${error.name}\n${error.message}`;
+      if (error.stack) parsedError += `\n${error.stack}`;
+
+      console.error(parsedError);
+
+      if (!fs.existsSync('./errors')) fs.mkdirSync('./errors');
+      fs.writeFileSync(`./errors/${filename}`, parsedError);
     }
   }
 };
