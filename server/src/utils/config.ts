477f11f46984eb87ca871961a7dca365b62873c1
d1r0l <xd1r0lx@gmail.com>
2024-07-07 17:57:47 +0300

    fix: :wrench: updated proxy and other configs

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import { isString, isNumber } from 'lodash';
 
 dotenv.config();
 
@@ -7,72 +6,60 @@ const envMode = process.env.NODE_ENV || 'development';
 const expressPort = process.env.PORT || 3000;
 const discordToken = process.env.DISCORD_TOKEN;
 
-const getIgCredentials = () => {
-  const igUser = process.env.IG_USERNAME;
-  const igPass = process.env.IG_PASSWORD;
+const igUser = process.env.IG_USERNAME;
+const igPass = process.env.IG_PASSWORD;
+const igCredentials =
+  igUser && igPass
+    ? {
+        username: igUser,
+        password: igPass,
+      }
+    : undefined;
 
-  if (isString(igUser) && isString(igPass))
-    return {
-      username: igUser,
-      password: igPass,
+interface Proxy {
+  protocol: string;
+  host: string;
+  port: number;
+  auth?: { username: string; password: string };
+}
+
+const parseProxy = () => {
+  const protocol = process.env.IG_PROXY_PROT;
+  const host = process.env.IG_PROXY_HOST;
+  const port = process.env.IG_PROXY_PORT
+    ? parseInt(process.env.IG_PROXY_PORT)
+    : undefined;
+  const username = process.env.IG_PROXY_USER;
+  const password = process.env.IG_PROXY_PASS;
+
+  if (!protocol || !host || !port) return undefined;
+  else {
+    const proxy: Proxy = {
+      protocol: protocol,
+      host: host,
+      port: port,
     };
-  else return undefined;
+    if (username && password) {
+      proxy.auth = {
+        username: username,
+        password: password,
+      };
+    }
+    return proxy;
+  }
 };
 
-const igCredentials = getIgCredentials();
+const proxy = parseProxy();
 
-const getProxy = () => {
-  const proxyProt = process.env.IG_PROXY_PROT;
-  const proxyHost = process.env.IG_PROXY_HOST;
-  const proxyPort = process.env.IG_PROXY_PORT;
-  const proxyUser = process.env.IG_PROXY_USER;
-  const proxyPass = process.env.IG_PROXY_PASS;
-
-  if (
-    isString(proxyProt) &&
-    isString(proxyHost) &&
-    isString(proxyPort) &&
-    isNumber(parseInt(proxyPort)) &&
-    isString(proxyUser) &&
-    isString(proxyPass)
-  )
-    return {
-      protocol: proxyProt,
-      host: proxyHost,
-      port: parseInt(proxyPort),
-      auth: {
-        username: proxyUser,
-        password: proxyPass,
-      },
-    };
-  else return undefined;
+const parseProxyUrl = () => {
+  if (!proxy) return undefined;
+  const urlParts = [proxy.protocol, '://'];
+  if (proxy.auth)
+    urlParts.push(proxy.auth.username, ':', proxy.auth.password, '@');
+  urlParts.push(proxy.host, ':', proxy.port.toString());
+  return urlParts.join('');
 };
 
-const axiosProxy = getProxy();
+const proxyUrl = parseProxyUrl();
 
-const getProxyUrl = () => {
-  if (axiosProxy) {
-    const proxyUrl =
-      axiosProxy.protocol +
-      '://' +
-      axiosProxy.auth.username +
-      ':' +
-      axiosProxy.auth.password +
-      '@' +
-      axiosProxy.host +
-      ':' +
-      axiosProxy.port;
-    return proxyUrl;
-  } else return undefined;
-};
-
-const proxyUrl = getProxyUrl();
-
-export {
-  envMode,
-  expressPort,
-  discordToken,
-  igCredentials,
-  axiosProxy,
-  proxyUrl,
-};
+export { envMode, expressPort, discordToken, igCredentials, proxy, proxyUrl };
