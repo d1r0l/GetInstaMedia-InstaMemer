2a9dc3d03fe7dafa274d348ac42068c1ca3cc806
d1r0l <xd1r0lx@gmail.com>
2024-07-14 02:35:00 +0300

    feat: :wrench: vite config refactor for using an enviroment variables

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,27 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import mkcert from 'vite-plugin-mkcert'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    mkcert({ savePath: '../certs' }),
-    VitePWA({ registerType: 'autoUpdate' })
-  ],
-  base: 'https://getinstamedia.d1r0l.xyz',
-  build: {
-    outDir: 'build'
-  },
-  server: {
-    port: 3001,
-    https: true,
-    proxy: {
-      '/api': {
-        target: 'https://192.168.1.5:3000',
-        changeOrigin: true,
-        secure: false
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [
+      react(),
+      mkcert({ savePath: '../certs' }),
+      VitePWA({ registerType: 'autoUpdate' })
+    ],
+    base: process.env.VITE_BASE_URL || '/',
+    build: {
+      outDir: 'build'
+    },
+    server: {
+      port: 3001,
+      proxy: {
+        '/api': {
+          target: process.env.VITE_DEV_BACK_URL || 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false
+        }
       }
     }
-  }
-})
+  })
+}
